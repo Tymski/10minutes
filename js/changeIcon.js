5aaba2974fa18e4e5bd7b57bf0a6938aabe48471
Tymski <tymon.oziemblewski@gmail.com>
2017-11-30 23:18:32 +0100

    Added more icons

--- js/changeIcon.js
+++ js/changeIcon.js
@@ -10,7 +10,11 @@
         'icon1_192.png',
         'icon2_192.png',
         'icon3_192.png',
-        'icon4_192.png'
+        'icon4_192.png',
+        'icon5_192.png',
+        'icon6_192.png',
+        'icon7_192.png',
+        'icon8_192.png'
     ];
     let index = Math.floor(Math.random() * imageNames.length); //0,1,2,3,4
     path += imageNames[index];
